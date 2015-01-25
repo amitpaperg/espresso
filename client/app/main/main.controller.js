f6428f8a1a1a1cd75048b8acb7667853602f4f28
Amit Velingkar <amit.velingkar@paperg.com>
2015-01-24 23:24:28 -0800

    Initial Framework

---
+++ client/app/main/main.controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('espressoApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $scope.awesomeThings = [];
+
+    $http.get('/api/things').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+
+  });
